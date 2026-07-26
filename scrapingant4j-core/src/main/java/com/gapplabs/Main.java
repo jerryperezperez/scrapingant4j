0fbf365f8fff2004f4cd358d73666ca9e5e3cf52
Gerardo Pérez Pérez <59417562+jerryperezperez@users.noreply.github.com>
2026-07-26 13:17:32 -0600

    Remove Main java file and its test file (#44)

--- scrapingant4j-core/src/main/java/com/gapplabs/Main.java
+++
@@ -1,8 +0,0 @@
-package com.gapplabs;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // Intentionally left blank. This module is a library, and the SDK is exercised via tests.
-    }
-}
